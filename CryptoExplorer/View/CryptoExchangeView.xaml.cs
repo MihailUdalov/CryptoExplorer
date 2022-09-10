0506890d8973796406dc2a850347c13847b08588
Удалов Михаил <misha2001dw@gmail.com>
2022-09-10 16:31:17 +0300

    Updated ExchangeView page

--- CryptoExplorer/View/CryptoExchangeView.xaml.cs
+++ CryptoExplorer/View/CryptoExchangeView.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CryptoExplorer.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CryptoExplorer.View
 {
@@ -20,9 +14,43 @@ namespace CryptoExplorer.View
     /// </summary>
     public partial class CryptoExchangeView : UserControl
     {
+        public CryptoCoins CryptoDataCoins { get; set; }
+
         public CryptoExchangeView()
         {
             InitializeComponent();
+
+            CryptoDataCoins = GetCryptoCoins();
+            FirstCryptoCoinComboBox.ItemsSource = CryptoDataCoins.Data;
+            SecondtCryptoCoinComboBox.ItemsSource = CryptoDataCoins.Data;
+        }
+
+        private CryptoCoins GetCryptoCoins()
+        {
+            const string URL = "https://api.coincap.io/v2/assets";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<CryptoCoins>(response);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (FirstCryptoCoinComboBox.SelectedItem is DataCoin firstCryptoCoin 
+                && SecondtCryptoCoinComboBox.SelectedItem is DataCoin secondCryptoCoin 
+                && Convert.ToDecimal(FirstTextBox.Text) is decimal count)
+                
+                SecondtTextBox.Text = Math.Round((Convert.ToDecimal(firstCryptoCoin.PriceUsd) * count) / Convert.ToDecimal(secondCryptoCoin.PriceUsd), 8).ToString();
+            
         }
     }
 }
